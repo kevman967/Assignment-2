b222c33f7ebc93c94b54fc38b2fefb6afd3d821c
kevman967 <kevman966@gmail.com>
2022-07-23 17:18:08 -0400

    master

--- edge.h
+++ edge.h
@@ -1,3 +1,4 @@
+#pragma once
 #include "vertex.h"
 class edge{
     private:
--- testing_vertex_and_edge.cpp
+++ testing_vertex_and_edge.cpp
@@ -1,6 +1,9 @@
 #include "edge.h"
 #include "vertex.h"
 int main(){
-    vertex one(5,5.4);
+    int id1;
+    double value1;
+    vertex one(id1,value1);
+
 
 }
